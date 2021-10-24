4f888f7cb3baef958d834e4bc28424af1e120696
Idoreyin Ankoh <iankoh@softalliance.co.ng>
2021-10-24 12:56:14 +0100

    new validations

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 //cache dom elements into variables for performance
-var firstName = $("#firstName");
-var lastName = $("#lastName");
-var email = $('#email');
-var male = $("#male");
-var female = $("#female");
-var other = $('#other');
-var password = $("#password");
-var confirmPass = $("#confirmPass");
-var xpLevel = $('#xpLevel');
-var create = $("#create");
+// var firstName = $("#firstName");
+// var lastName = $("#lastName");
+// var email = $('#email');
+// var male = $("#male");
+// var female = $("#female");
+// var other = $('#other');
+// var password = $("#password");
+// var confirmPass = $("#confirmPass");
+// var xpLevel = $('#xpLevel');
+// var create = $("#create");
 
 //trigger checking if anything changes in textbox or checkox
 // firstName.bind("keyup change", checker);
@@ -22,26 +22,72 @@ var create = $("#create");
 // xpLevel.bind("keyup change", checker);
 
 
-jQuery(function($) {
-    var $fields = $('#firstname, #lastName, #email, #password, #confirmPass, #xpLevel ');
+// jQuery(function($) {
+//     var $fields = $('#firstname, #lastName, #email, #password, #confirmPass, #xpLevel ');
 
-    $fields.on('input', function() {
-        if (allFilled($fields)) {
-            $('#create').removeAttr('disabled');
-            $('#create').addClass('enabled');
+//     $fields.on('input', function() {
+//         if (allFilled($fields)) {
+//             $('#create').removeAttr('disabled');
+//             $('#create').addClass('enabled');
+//         } else {
+//             // $('#create').addAttr('disabled');
+//             $('#create').removeClass('enabled')
+//         }
+//     });
+
+//     function allFilled($fields) {
+//         return $fields.filter(function() {
+//             return this.value === '';
+//         }).length == 0;
+//     }
+// });
+// var count = document.getElementsByTagName("input").length;
+// for (let index = 0; index < count; index++) {
+//     var textState = this.value;
+//     if (textState > 0) {
+//         console.log("not")
+//     } else {
+//         console.log("empty")
+//     }
+
+// }
+
+
+document.addEventListener("click", check);
+
+function check() {
+    var firstName = document.querySelector('#firstName').value.length;
+    var lastName = document.querySelector('#lastName').value.length;
+    var email = document.querySelector('#email').value.length;
+    var male = document.querySelector("#male").value.length;
+    var female = document.querySelector("#female").value.length;
+    var other = ('#other');
+    var password = document.querySelector("#password").value.length;
+    var confirmPass = document.querySelector("#confirmPass").value.length;
+    var xpLevel = document.querySelector('#xpLevel').value.length;
+    var create = document.querySelector("#create").value.length;
+    var count = document.getElementsByTagName("input").length;
+    for (let index = 0; index < count; index++) {
+        var textState = document.getElementsByTagName("input")[index].value.length;
+        // var o = document.getElementsByTagName("input")[index].value;
+        //console.log(textState);
+        if (firstName > 0 && lastName > 0 && email > 0 && password > 0 && confirmPass > 0 && xpLevel > 0) {
+            // console.log("not")
+            // console.log(o);
+            console.log("not")
+            document.getElementById("create").disabled = false;
+            document.querySelector('#create').classList.add('enabled');
+
         } else {
-            $('#create').addAttr('disabled');
-            $('#create').removeClass('enabled')
+            console.log("empty")
+            document.getElementById("create").disabled = true;
+            document.querySelector('#create').classList.remove('enabled');
+
+
         }
-    });
 
-    function allFilled($fields) {
-        return $fields.filter(function() {
-            return this.value === '';
-        }).length == 0;
     }
-});
-
+}
 
 
 
